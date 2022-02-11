855e13f916c809cd72890ebe2417a69a691ea298
NakRex <khannasimahmad@gmail.com>
2022-02-11 11:15:06 +0530

    added route

---
+++ routes/user.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const userRouter = express.Router();
+const controller = require('../controller/usersController')
+
+userRouter.post('/login', controller.login)
+userRouter.post('register', controller.signin)
+userRouter.post('/upload', controller.uploadPhoto)
