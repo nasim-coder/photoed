525ee3f565ce91ed42f7b8752321ca3124415844
NakRex <khannasimahmad@gmail.com>
2022-02-12 13:12:26 +0530

    fixed errors

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const userRouter = express.Router();
 const controller = require('../controller/usersController');
-const upload = require('../middleware/gridFsEngine')
+const {upload} = require('../middleware/gridFsEngine')
 
 userRouter.post('/login', controller.login);
-userRouter.post('register', controller.signin);
+userRouter.post('/register', controller.signUp);
 userRouter.post('/upload', upload.single('file'), controller.addPhoto);
+
+module.exports = userRouter;
--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const express = require('express');
-const env = require('dotenv');
-env.config();
+const userRouter = require('./routes/user') 
 const app = express();
 const PORT = process.env.PORT||3000;
 const mongose = require('mongoose');
 mongose.connect('mongodb://localhost:27017/photodb', () => {
     console.log('db connected');
-})
+});
 
 app.use(express.json());
-
+app.use('/user', userRouter)
 app.listen(PORT, function(){
 console.log("server is running on port 3000");
 })
