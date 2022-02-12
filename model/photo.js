9118eba0556631703951d77d1895a9dcf09f40dd
NakRex <khannasimahmad@gmail.com>
2022-02-12 12:02:51 +0530

    added Photo model

--- model/photo.js
+++ model/photo.js
@@ -3,14 +3,21 @@ let photoSchema = new mongoose.Schema({
     caption: {
         type: String,
     },
-    ownerId: {
+    userId: {
         type: mongoose.Schema.Types.ObjectId,
-       ref: 'User' 
+        ref: 'User' 
     },
     likedby: [
         {
             type: mongoose.Schema.Types.ObjectId,
             ref: 'User'
         }
-    ]
+    ],
+    fileId: {
+        type: mongoose.Types.ObjectId,
+        required:true
+    }
 })
+
+let Photo = mongoose.model('Photo', photoSchema);
+module.exports = Photo;
