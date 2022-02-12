bf956d51a2c9256e0020a53764f0b2d83dd0baa8
NakRex <khannasimahmad@gmail.com>
2022-02-12 20:29:48 +0530

    added Comment model

---
+++ model/comments.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+let commentSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    comment: {
+        type: String,
+        required: true
+    },
+
+})
+
+let Comment = mongoose.model('Comment', commentSchema);
+module.exports = Comment;
