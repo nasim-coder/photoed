3faeef8d6767aef4c68b0346e6334fb6e6b33fbc
NakRex <khannasimahmad@gmail.com>
2022-02-12 11:51:15 +0530

    added login fuction

--- controller/usersController.js
+++ controller/usersController.js
@@ -16,3 +16,19 @@ exports.signUp = (req, res) => {
     });
 
 };
+//login function for the user and assigning token
+exports.login = async (req, res) => {
+    const { email, password } = req.body;
+    if (!(email && password)) {
+        res.status(400).send("All input is required");
+    }
+    const user = await User.findOne({ email });
+    let isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (user && (isPasswordCorrect)) {
+        const token = jwt.sign({ user }, jwtconfig.secret, { "expiresIn": "2h" });
+        user.token = token;
+        res.status(200).json(user)
+    } else {
+        res.status(400).send("invalid credential")
+    }
+}
