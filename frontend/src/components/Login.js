078a30aa5677985c85882d49d42809e3063f6d4c
NakRex <khannasimahmad@gmail.com>
2022-02-23 20:55:29 +0530

    completed login component

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -2,7 +2,11 @@ function Login() {
     return (
         <div className="login">
             <form className="login-form">
-            
+            <label htmlFor="email">Email</label>
+                <input type="email" name="email" id="email" />
+            <label htmlFor="password">Password</label>
+                <input type="password" name="password" id="password" />
+            <input type="submit" value="LOGIN" />
             </form>
         </div>
     );
