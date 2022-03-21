967cb8c3a557a905a9e8d891630d0217389743e9
NakRex <khannasimahmad@gmail.com>
2022-03-21 21:19:57 +0530

    fixed register component

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,11 +1,17 @@
-function Register() {
-    return (
-        <form action="">
-            <input type="text" name='name' placeholder="enter name" onChange={function (e) { }} />
-            <input type="email" name="email" id="" placeholder="entr email" />
-            <input type="password" name="password" id="" placeholder="enter password" />
-            <input type="button" value="Register" />
-        </form>
-    );
+import React, { Component } from 'react';
+
+class Register extends Component() {
+    render() {
+        return (
+            <div>
+                <form action=''>
+                    <input type="text" name='name' placeholder="enter name" onChange={function (e) { }} />
+                    <input type="email" name="email" id="" placeholder="entr email" />
+                    <input type="password" name="password" id="" placeholder="enter password" />
+                    <input type="button" value="Register" />
+                </form>
+            </div>
+        );
+    }
 }
 export default Register;
